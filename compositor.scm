7f023cecd9b94dab2d005558b6b1d44e8841e84b
Johan Engstrand <johan@engstrand.nu>
2021-11-28 16:06:52 +0100

    feat: add separate guile scripts for user and compositor status

---
+++ compositor.scm
@@ -0,0 +1,80 @@
+#!/gnu/store/18hp7flyb3yid3yp49i6qcdq0sbi5l1n-guile-3.0.2/bin/guile --no-auto-compile
+!#
+
+(use-modules (ice-9 popen)
+             (ice-9 rdelim)
+             (srfi srfi-1)
+             (rnrs arithmetic bitwise))
+
+(define %workspaces 9)
+(define %monitor "DP-1")
+
+(define (update-blocks status-info blocks)
+  (fold (lambda (block acc)
+          (let ((str (block status-info)))
+            (if (string? str)
+                (if (eq? (string-length acc) 0)
+                    str
+                    (string-append acc " " str))
+                acc)))
+        ""
+        blocks))
+
+;; TODO: Respect dwl-guile workspace names
+(define (%block-tags info)
+  (let* ((dpy (assoc-ref info %monitor))
+         (selected-tag (assoc-ref dpy "selected-tag")))
+    (fold-right (lambda (tag acc)
+                  (let ((selected (and (number? selected-tag)
+                                       (logbit? (- tag 1) selected-tag))))
+                    (string-append
+                      (format #f "~a ~a ~a"
+                              (if selected "^bg(#FF0000)^fg(#FFFFFF)" "")
+                              (number->string tag)
+                              (if selected "^bg()^fg()" ""))
+                      acc)))
+                ""
+                (iota %workspaces 1))))
+
+(define (%block-title info)
+  (unless (null? info)
+    (let* ((dpy (assoc-ref info %monitor))
+           (title (assoc-ref dpy "title")))
+      (when (string? title)
+        (string-append title)))))
+
+(define %compositor-blocks
+  (list
+    %block-tags
+    %block-title))
+
+(define read-port (current-input-port))
+(define write-port (open-output-pipe "dtao -a -ta l -fg FFFFFF -z -z -fn \"JetBrains Mono:style=bold:size=13\""))
+
+(define (update-compositor-blocks blocks status-info)
+  (let loop ((line (read-line)))
+    (if (char-ready? read-port)
+        (loop (read-line)
+              (let* ((line-data (string-split line #\space))
+                     (dpy (car line-data))
+                     (saved-info (or (assoc-ref status-info dpy) '())))
+                (assoc-set!
+                  status-info
+                  dpy
+                  (let ((type (cadr line-data)))
+                    (if (eq? type "tags")
+                        (append `(("active-tags" . ,(caddr line-data))
+                                  ("selected-tag" . ,(cadddr line-data)))
+                                saved-info)
+                        (cons `(,type . ,(caddr line-data))
+                              saved-info))))))
+        (update-blocks status-info blocks))))
+
+(define (statusbar initial-state)
+  (let loop ((previous-base-output (%block-tags '()) current-state))
+    (begin
+      (display (string-append "^tw()" previous-base-output "\n") write-port)
+      (loop (update-compositor-blocks %compositor-blocks current-state) current-state))))
+
+(statusbar '())
+(close-pipe write-port)
---
+++ user.scm
@@ -0,0 +1,51 @@
+#!/gnu/store/18hp7flyb3yid3yp49i6qcdq0sbi5l1n-guile-3.0.2/bin/guile --no-auto-compile
+!#
+(use-modules (ice-9 popen)
+             (ice-9 rdelim)
+             (srfi srfi-1))
+
+(define (update-blocks status-info blocks)
+  (fold (lambda (block acc)
+          (let ((str (block status-info)))
+            (if (string? str)
+                (string-append acc " " str)
+                acc)))
+        ""
+        blocks))
+
+(define (%block-time info)
+  (let* ((port (open-input-pipe "date \"+%a %d %b (v.%V) %T \""))
+         (result (read-line port)))
+    (close-pipe port)
+    (string-append "^ca(0,notify-send \"Hello world\")" result "^ca()")))
+
+(define (%block-battery info)
+  (let* ((port (open-input-file "/sys/class/power_supply/BAT0/capacity"))
+         (result (read-line port)))
+    (close-port port)
+    (string-append "^fg(#00FF00)"result "%")))
+
+(define (%block-gibberish info)
+  "BREAKING NEWS: Fredrik fuckade upp igen")
+
+(define %user-blocks
+  (list
+    %block-time))
+
+(define write-port (open-output-pipe "dtao -a -ta l -sa c -fg FFFFFF -fn \"JetBrains Mono:style=bold:size=13\""))
+
+(define (update-user-blocks blocks iteration)
+  (update-blocks '() blocks))
+
+(define (statusbar)
+  (let loop ((iteration 0) (previous-user-output ""))
+    (begin
+      (let* ((user-output (update-user-blocks %user-blocks iteration)))
+        (unless (eq? previous-user-output user-output)
+          (display (string-append "^sw()" user-output "\n")
+                   write-port))
+        (sleep 1)
+        (loop (+ iteration 1) user-output)))))
+
+(statusbar)
+(close-pipe write-port)
