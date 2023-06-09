c3378ed8d1116dab4fc21ee3a8d021a8cecb7069
Fredrik Engstrand <fredrik@engstrand.nu>
2023-06-09 19:11:57 +0200

    feat: add more config helper implementations

--- share/init.scm
+++ share/init.scm
@@ -6,6 +6,38 @@
    (make-exception-with-message
     (string-append "dtao: " message))))
 
+(define* (dtao:block-group
+          #:key
+          (flex 1)
+          (delimiter #f)
+          (align 'left)
+          (bg-color "#000000")
+          (fg-color "#FFFFFF")
+          (content '()))
+  "Creates a block group inside a layout.
+The group will contain CONTENT, which can be either a list of
+block names, or a function. Content inside the group will be separated
+by DELIMITER and aligned inside the group according to ALIGN. If FLEX is
+set to true, the group will expand as much as possible. Increasing FLEX
+to values larger than other block groups will result in it expanding more
+compared to others. BG-COLOR and FG-COLOR are used to set the default
+colors for content inside the group, including the group area itself."
+  (unless (or (list? content) (procedure? content))
+    (raise-error "expected content to be a list or function"))
+
+  (when (procedure? content)
+      (unless (eq? (car (procedure-minimum-arity content) 1))
+        (raise-error "content function expects one argument (monitor)")))
+
+  `((flex . ,flex)
+    ,@(if (eq? delimiter #f)
+          '()
+          `((delimiter . ,delimiter)))
+    (align . ,align)
+    (bg-color . ,bg-color)
+    (fg-color . ,fg-color)
+    (content . ,content)))
+
 (define* (dtao:start-repl-server)
   "Starts a local Guile REPL server, listening on a UNIX socket at path
 @path{/tmp/dtao-guile.socket}. This REPL allows you to execute expressions
@@ -29,7 +61,7 @@ be started!"
   (kill-server)
   (spawn-server (make-unix-domain-server-socket #:path dtao:%repl-socket-path)))
 
-(define* (define-block id
+(define* (dtao:make-block id
            #:key
            (signal 0)
            (interval 0)
@@ -45,7 +77,7 @@ the block will be updated with the new properties."
 
   (when click
     (unless (eq? (car (procedure-minimum-arity click)) 1)
-      (raise-error "click procedure expects one argument (monitor).")))
+      (raise-error "click procedure expects two arguments (monitor, button).")))
 
   (set 'blocks
        `((id . ,(format #f "~a" id))
@@ -54,3 +86,22 @@ the block will be updated with the new properties."
          (events? . ,events?)
          (render . ,render)
          (click . ,click))))
+
+(define* (dtao:make-layout id layout)
+  "Defines a layout with id ID. LAYOUT should be a list of
+block groups and/or block ids. If multiple layouts are used,
+you can select which layout to render using @code{dtao:set-renderer}.
+If no renderer is set, the first created layout will be used."
+  (unless (list? layout)
+    (raise-error "layout should be a list"))
+  (set 'layouts
+       `((id . ,(format #f "~a" id))
+         (layout . ,layout))))
+
+(define* (dtao:set-renderer func)
+  "Configures a custom renderer function used to
+select which layout to render for the current state."
+  (unless (eq? (car (procedure-minimum-arity func)) 2)
+    (raise-error "renderer function expectes two arguments \
+(monitor, current-state)"))
+  (set 'renderer func))
