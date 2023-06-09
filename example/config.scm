035c75448858005f8efab5ce8c76ab59b0b48185
Fredrik Engstrand <fredrik@engstrand.nu>
2023-06-09 19:12:29 +0200

    feat: add example config

--- example/config.scm
+++ example/config.scm
@@ -1,108 +1,87 @@
-(begin 
-  (use-modules 
-   (ice-9 match)
-   (ice-9 popen)
-   (ice-9 rdelim)
-   (srfi srfi-1))
-  (define config 
-    (quasiquote 
-     (("blocks") 
-      ("left-blocks" 
-       (("render" lambda () (cond ((dtao:selected-tag? 0) "^bg(#ffcc00)^fg(#191919)^p(8)1^p(8)^fg()^bg()") ((dtao:urgent-tag? 0) "^bg(#ff0000)^fg(#ffffff)^p(8)1^p(8)^fg()^bg()") ((dtao:active-tag? 0) "^bg(#323232)^fg(#ffffff)^p(8)1^p(8)^fg()^bg()") (else "^p(8)1^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 0))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 1) "^bg(#ffcc00)^fg(#191919)^p(8)2^p(8)^fg()^bg()") ((dtao:urgent-tag? 1) "^bg(#ff0000)^fg(#ffffff)^p(8)2^p(8)^fg()^bg()") ((dtao:active-tag? 1) "^bg(#323232)^fg(#ffffff)^p(8)2^p(8)^fg()^bg()") (else "^p(8)2^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 1))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 2) "^bg(#ffcc00)^fg(#191919)^p(8)3^p(8)^fg()^bg()") ((dtao:urgent-tag? 2) "^bg(#ff0000)^fg(#ffffff)^p(8)3^p(8)^fg()^bg()") ((dtao:active-tag? 2) "^bg(#323232)^fg(#ffffff)^p(8)3^p(8)^fg()^bg()") (else "^p(8)3^p(8)"))) 
-        ("click" lambda (button) (match button (0 (dtao:view 2))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 3) "^bg(#ffcc00)^fg(#191919)^p(8)4^p(8)^fg()^bg()") ((dtao:urgent-tag? 3) "^bg(#ff0000)^fg(#ffffff)^p(8)4^p(8)^fg()^bg()") ((dtao:active-tag? 3) "^bg(#323232)^fg(#ffffff)^p(8)4^p(8)^fg()^bg()") (else "^p(8)4^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 3))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 4) "^bg(#ffcc00)^fg(#191919)^p(8)5^p(8)^fg()^bg()") ((dtao:urgent-tag? 4) "^bg(#ff0000)^fg(#ffffff)^p(8)5^p(8)^fg()^bg()") ((dtao:active-tag? 4) "^bg(#323232)^fg(#ffffff)^p(8)5^p(8)^fg()^bg()") (else "^p(8)5^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 4))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 5) "^bg(#ffcc00)^fg(#191919)^p(8)6^p(8)^fg()^bg()") ((dtao:urgent-tag? 5) "^bg(#ff0000)^fg(#ffffff)^p(8)6^p(8)^fg()^bg()") ((dtao:active-tag? 5) "^bg(#323232)^fg(#ffffff)^p(8)6^p(8)^fg()^bg()") (else "^p(8)6^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 5))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 6) "^bg(#ffcc00)^fg(#191919)^p(8)7^p(8)^fg()^bg()") ((dtao:urgent-tag? 6) "^bg(#ff0000)^fg(#ffffff)^p(8)7^p(8)^fg()^bg()") ((dtao:active-tag? 6) "^bg(#323232)^fg(#ffffff)^p(8)7^p(8)^fg()^bg()") (else "^p(8)7^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 6))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 7) "^bg(#ffcc00)^fg(#191919)^p(8)8^p(8)^fg()^bg()") ((dtao:urgent-tag? 7) "^bg(#ff0000)^fg(#ffffff)^p(8)8^p(8)^fg()^bg()") ((dtao:active-tag? 7) "^bg(#323232)^fg(#ffffff)^p(8)8^p(8)^fg()^bg()") (else "^p(8)8^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 7))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () 
-         (cond ((dtao:selected-tag? 8) "^bg(#ffcc00)^fg(#191919)^p(8)9^p(8)^fg()^bg()") ((dtao:urgent-tag? 8) "^bg(#ff0000)^fg(#ffffff)^p(8)9^p(8)^fg()^bg()") ((dtao:active-tag? 8) "^bg(#323232)^fg(#ffffff)^p(8)9^p(8)^fg()^bg()") (else "^p(8)9^p(8)")))
-        ("click" lambda (button) (match button (0 (dtao:view 8))))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0))
-       (("render" lambda () (string-append "^p(4)" (dtao:get-layout)))
-        ("click" lambda (button) (dtao:next-layout))
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0)))
-      ("center-blocks" 
-       (("render" lambda () (dtao:title))
-        ("click" . #f)
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #t)
-        ("interval" . 0)))
-      ("right-blocks" 
-       (("render" lambda () (strftime "%A, %d %b (w.%V) %T" (localtime (current-time))))
-        ("click" . #f)
-        ("position" . "right")
-        ("signal" . #f)
-        ("events" . #f)
-        ("interval" . 1)))
-      ("height" . #f)
-      ("font" . "monospace:style=bold:size=12")
-      ("background-color" . "111111AA")
-      ("border-color" . "333333FF")
-      ("foreground-color" . "FFFFFFFF")
-      ("border-px" . #f)
-      ("exclusive" . #t)
-      ("bottom" . #f)
-      ("padding-top" . 2)
-      ("padding-bottom" . 2)
-      ("padding-left" . 8)
-      ("padding-right" . 8)
-      ("adjust-width" . #f)
-      ("delimiter" . #f)
-      ("block-spacing" . 0)
-      ("layer" . LAYER-BOTTOM)))))
+;; General settings
+(setq background-color "#000000"
+      text-color "#FFFFFF"
+      position 'top
+      font "..."
+      height 25
+      layer 'LAYER-OVERLAY
+      padding-left 0
+      padding-right 0
+      padding-top 0
+      padding-bottom 0)
+
+(dtao:make-block 'tags
+  #:events? #t
+  #:render
+  (lambda (monitor)
+    <render>))
+
+(dtao:make-block 'layout
+  #:events? #t
+  #:render
+  (lambda (monitor)
+    (dtao:monitor-layout monitor)))
+
+(dtao:make-block 'title
+  #:events? #t
+  #:render
+  (lambda (monitor)
+    (dtao:monitor-title monitor)))
+
+(dtao:make-block 'battery
+  #:interval 30
+  #:render
+  (lambda (_)
+    <render>))
+
+(dtao:make-block 'time
+  #:interval 1
+  #:render
+  (lambda (_)
+    <render>))
+
+(dtao:make-block 'volume
+  #:signal 10 ;; Re-render on signal
+  #:render
+  (lambda (_)
+    <render>))
+
+(dtao:make-block 'error-message
+  #:events? #t
+  #:click (lambda (_) <action>)
+  #:render
+  (lambda (_)
+    (dtao:get-dwl-error-message)))
+
+(dtao:make-layout
+ 'default-layout
+ (list
+  (dtao:block-group
+   #:content '(tags layout))
+  (dtao:block-group
+   #:align 'center
+   #:content '(title))
+  (dtao:block-group
+   #:delimiter " "
+   #:align 'right
+   #:content
+   (lambda (monitor)
+     (if (dtao:monitor-focused monitor)
+         '(battery time)
+         '())))))
+
+(dtao:make-layout
+ 'error-layout
+ (list
+  (dtao:block-group
+   ;; Override colors for this group
+   #:bg-color "#FF0000"
+   #:fg-color "#FFFFFF"
+   #:content '(error-message))))
+
+(dtao:set-renderer
+  (lambda (monitor current-state)
+    (if (and (dtao:monitor-focused monitor)
+             (eq? current-state 'error))
+        'error-layout
+        'default-layout)))
